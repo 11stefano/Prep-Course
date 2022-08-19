d28136d42c068465fb427739c8113bd2e3c2a41c
Stefano <sstefanoarmeli@gmail.com>
2022-08-19 17:08:26 -0300

    first step js homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -27,28 +27,28 @@ const nuevoModulo = 21 % 5 === 1;
 
 function devolverString(str) {
   // "Return" la string provista: str
-  // Tu código:
+  // Tu código: 
   
-} return str;
+  return str; } 
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
   
-} return (x + y);
+  return (1 + 2); }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
   
-} return (x - y );
+  return (1 - 2 ); } 
  
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
   
-} return (x * y);
+  return (x * y); }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
